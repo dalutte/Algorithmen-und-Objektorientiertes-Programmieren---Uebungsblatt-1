36866df87e426a6adf5050a037c0ca167a47860f
Clemens <Clemens@CLEMENS-PC>
2023-03-07 11:14:16 +0100

    Projektdateien hinzufügen.

---
+++ Übungsblatt 1/Aufgabe2.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+using System; 
+
+internal class Program
+{
+    //private static int[] Agen(int n, int Og)
+    //{
+    //    int[] num1 = new int[n];
+    //    Random r = new Random();
+
+    //    for (int i = 1; i <= Og; i++)
+    //    {
+    //        for (int j = 0; j < n; j++)
+    //        {
+    //            num1[j] = r.Next(1,n);
+    //        }
+    //    }
+    //    return num1;
+    //}
+
+    private static bool Check(int[] list)
+    {
+            int[] found = new int[10];
+
+            int i = 0;
+            foreach (int item in list)
+            {
+                if (item >= 1 && item <= 10)
+                {
+                    found[i] = item;
+                    i++;
+                    Console.WriteLine(item);
+                }
+            }
+
+            if (found.Length < 10)
+            {
+                return false;
+            }
+            return true;
+    }
+
+
+    private static void Main(string[] args)
+    {
+            //int Arraylänge = 16;
+            //int Obergrenze = 20;
+            //int[] Liste = Agen(Arraylänge, Obergrenze);
+            //Console.WriteLine(Agen(Arraylänge,Obergrenze
+
+            Console.WriteLine(Check(Liste));
+    }
+    
+}
